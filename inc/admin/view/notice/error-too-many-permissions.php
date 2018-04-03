6a09d49397b54240fdb9807c23b116d1ba520c48
Aaron Rutley <aaron.rutley@gmail.com>
2018-04-03 15:51:21 +1000

    revise the token permissions mesaage so it's on two lines

--- inc/admin/view/notice/error-too-many-permissions.php
+++ inc/admin/view/notice/error-too-many-permissions.php
@@ -8,5 +8,6 @@
 
 ?>
 <div class="notice notice-error is-dismissible">
-	<p><?php printf( esc_html__( 'Incorrect token permissions. Please ensure only %s permissions are enabled.', 'envato-market' ), sprintf( '"%s"', implode( '", "', $this->get_required_permissions() ) ) ); ?></p>
+	<p><?php printf( esc_html__( 'Incorrect token permissions.' )); ?></p>
+	<p><?php printf( esc_html__( 'Please ensure only the following %s permissions are enabled.', 'envato-market' ), sprintf( '"%s"', implode( '", "', $this->get_required_permissions() ) ) ); ?></p>
 </div>
