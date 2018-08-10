cbf4837f71ecac8354f78466543a7bc0d0d10d73
dtbaker <dtbaker@gmail.com>
2018-08-10 16:03:53 +1000

    Change help page content.

--- inc/admin/view/partials/help.php
+++ inc/admin/view/partials/help.php
@@ -11,7 +11,7 @@
 <div id="help" class="panel">
 	<div class="envato-market-blocks">
 		<div class="envato-market-block">
-			<h3>Help</h3>
+			<h3>Help:</h3>
 			<p>When help is required using this plugin please follow these steps:</p>
 			<ol>
 				<li>First check the FAQ to see if the problem can be solved.</li>
@@ -22,11 +22,11 @@
 				<li>Check with the item author to confirm the Theme or Plugin is compatible with the Envato Market plugin.</li>
 				<li>Confirm a valid API token has been generated from the <a href="<?php echo envato_market()->admin()->get_generate_token_url();?>" target="_blank">build.envato.com</a> website.</li>
 				<li>Confirm your Envato account is still active and the items are still visible from <a href="https://themeforest.net/downloads" target="_blank">your downloads page</a>.</li>
-				<li>If this still doesn't solve the issue, please <a href="mailto:marketpluginsupport@envato.com">send us an email</a>. We may ask for your Envato username, the item you are trying to install/update, and some temporary WordPress login details so we can debug the issue. </li>
+				<li>If this still doesn't solve the issue, please send us an email to <a href="mailto:marketpluginsupport@envato.com">marketpluginsupport@envato.com</a>. We may ask for your Envato username, the item you are trying to install/update, and some temporary WordPress login details so we can debug any issues. </li>
 			</ol>
 		</div>
 		<div class="envato-market-block">
-			<h3>FAQ:</h3>
+			<h3>Common Problems:</h3>
 			<p>Common problems and how to solve them.</p>
 			<ol>
 				<li>
