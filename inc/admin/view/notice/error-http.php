645112d19ea91f07dcbbae43f3259d787554a94b
dtbaker <dtbaker@gmail.com>
2019-09-30 12:42:31 +1000

    Update http error message to mention TLS version

--- inc/admin/view/notice/error-http.php
+++ inc/admin/view/notice/error-http.php
@@ -8,5 +8,5 @@
 
 ?>
 <div class="notice notice-error is-dismissible">
-	<p><?php esc_html_e( 'Failed to connect to the Envato API. Please try again and if the issue persists contact the hosting providier to ask: "Please un-block PHP connections from WordPress to api.envato.com".', 'envato-market' ); ?></p>
+	<p><?php esc_html_e( 'Failed to connect to the Envato API. Please confirm with the hosting provider that the WordPress install has permission to communicate with api.envato.com and they support TLS version 1.2 and above. The hosting provider can assist with this.', 'envato-market' ); ?></p>
 </div>
