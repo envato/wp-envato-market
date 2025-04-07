025f2cf8c25a428fa13af5775c93c82368fd94a2
Alex Lion <learnwithalex@gmail.com>
2025-04-07 11:50:40 -0700

    Improve I18N Issues Based on 2.0.12

--- inc/admin/view/notice/error-permissions.php
+++ inc/admin/view/notice/error-permissions.php
@@ -8,8 +8,8 @@
 
 ?>
 <div class="notice notice-error is-dismissible">
-	<p><?php printf( esc_html__( 'Incorrect token permissions, please generate another token or fix the permissions on the existing token.' ) ); ?></p>
-	<p><?php printf( esc_html__( 'Please ensure only the following permissions are enabled: ', 'envato-market' ) ); ?></p>
+	<p><?php esc_html_e( 'Incorrect token permissions, please generate another token or fix the permissions on the existing token.', 'envato-market' ); ?></p>
+	<p><?php esc_html_e( 'Please ensure only the following permissions are enabled: ', 'envato-market' ); ?></p>
 	<ol>
 		<?php foreach ( $this->get_required_permissions() as $permission ) { ?>
 				<li><?php echo esc_html( $permission ); ?></li>
